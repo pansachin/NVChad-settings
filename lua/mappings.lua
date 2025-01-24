6ae34f06a4027755d23c63490db7ab9a82bc1f87
Sachin Prasad <sachin.prasad@pantheon.io>
2025-01-24 11:39:25 +0530

    Terminal escape mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,7 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- exit terminal mode
--- map("t", "<ESC>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+map("t", "<leader><ESC>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 --
